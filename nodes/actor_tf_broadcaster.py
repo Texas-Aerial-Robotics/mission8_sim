003a98a5dc10d7d2e657837a0cdb3fd72b701978
RICHARDO <danielespinoza@utexas.edu>
2019-05-03 16:25:36 -0500

    started msg and tf scripts for the actor

---
+++ nodes/actor_tf_broadcaster.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python  
+import roslib
+roslib.load_manifest('mission8_sim')
+import rospy
+import tf
+import actor.msg
+def handle_actor_pose(msg, actor):
+    br = tf.TransformBroadcaster()
+    br.sendTransform((msg.x, msg.y, 0),
+                     tf.transformations.quaternion_from_euler(0, 0, msg.theta),
+                     rospy.Time.now(),
+                     actor,
+                     "world")
+
+if __name__ == '__main__':
+    rospy.init_node('actor_broadcaster')
+    actorname = rospy.get_param('~actor')
+    rospy.Subscriber('/%s/pose' % actor,
+                     actor.msg.Pose,
+                     handle_actor_pose,
+                     actor)
+    rospy.spin()
+throwerrormessagebiatch
---
+++ nodes/acttf2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import rospy
+
+import tf_conversions
+
+import tf2_ros
+import geometry_msgs.msg
+import /../msg/actor.msg
